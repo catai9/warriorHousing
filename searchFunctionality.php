795e1f8f8a26adc372801784eed65241b23a875b
sehersarin <sarin.seher@gmail.com>
2020-03-14 18:52:25 -0400

    up1

--- searchFunctionality.php
+++ searchFunctionality.php
@@ -1,65 +1,91 @@
+<!-- TO DO: CHANGE BELOW COMMENTS -->
+<!-- Page 3 -->
+<!-- Page Details - Buyer Home Page -->
+
+<head>
+  <title>buyerHome</title>
+</head>
 <?php
 
-if (isset($_POST['search']))
-{
-    $valueToSearch = $_POST['valueToSearch'];
-    $query = "SELECT * FROM `Rental_Listing` WHERE Street_Name LIKE = '%".$valueToSearch."%'";
-    $search_result =filterTable($query);
-}
-else{
-    $query="SELECT `Rental_Listing_ID`,`House_Number`,`Street_Name`,`City`,`Rent_Per_Person`,`Vacancies` FROM `Rental_Listing`";
-    $search_result =filterTable($query);
-}
-
-function filterTable($query)
-{
-    $connect = mysqli_connect("localhost","root","root","warrior_ housing","3306");
-    $filter_Result = mysqli_query($connect, $query);
-    return $filter_Result;
-}
+// $User_ID = $_Get["User_ID"]; -->
 
 ?>
+<body>
+	<h1>Warrior Housing</h1>
+	
+<!-- TO DO: CHANGE REDIRECT PAGE-->
+	<!-- Which page it will direct go upon submitting the form. -->
+	<!-- If the form submission is successful, it will redirect to its respective php file -->
+	<form action="searchListing.php" method="post"
+		$User_ID
+		<br>
+			<!-- The button for search -->
+			<input type="submit" value="Search"/>
+		</br>
+	</form>
+	<form action="filterListing.php" method="post"
+		$User_ID
+		<br>
+			<!-- The button for filter -->
+			<input type="submit" value="Filter"/>
+		</br>
+	</form>
+	<form action="sortListing.php" method="post"
+		$User_ID
+		<br>
+			<!-- The button for this form. -->
+			<input type="submit" value="Sort"/>
+		</br>
+	</form>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Search Functionality</title>
-        <style>
-            table,tr,th,td
-            {
-                border: 1px solid black;
+	<?PHP
+			// Enable error logging: 
+			error_reporting(E_ALL ^ E_NOTICE);
+			// mysqli connection via user-defined function
+
+			include('./my_connect.php');
+			$mysqli = get_mysqli_conn();
+
+		
+			// SQL statement
+			$sql = "SELECT r.rental_listing_ID, r.city, r.street_name, r.house_number, r.vacancies, r.rent_per_person, r.availability_length
+            FROM rental_listing r
+            ORDER BY r.rent_per_person ASC
+            LIMIT 5";
+
+			// Prepared statement, stage 1: prepare
+			$stmt = $mysqli->prepare($sql);
+
+			// Prepared statement, stage 2: execute
+			$stmt->execute();
+
+			// Bind result variables 
+			$stmt->bind_result($rental_listing_ID, $city, $street_name, $house_number, $vacancies, $rent_per_person, $availability_length); 
+		
+			//printing output in html table
+            echo '<table style="width:100%">';
+            echo '<tr>';
+                echo '<th>Rental Listing ID</th>';
+                echo '<th>City</th>';
+                echo '<th>Street Name</th>';
+                echo '<th>House Number</th>';
+                echo '<th>Vacancies</th>';
+                echo '<th>Rent Per Person</th>';
+                echo '<th>Length of Availability</th>';
+            echo '</tr>';
+            while ($stmt->fetch()) {
+            echo '<tr><td>' . $rental_listing_ID . '</td><td>' . $city . '</td><td>' . $street_name . '</td><td>'. $house_number . '</td><td>'. $vacancies . '</td><td>'. $rent_per_person . '</td><td>'. $availability_length. '</td><td>';
             }
-        </style>
-    </head>
-    <body>
-        
-        <form action="searchFunctionality.php" method="post">
-            <input type="text" name="valueToSearch" placeholder="Value To Search"><br><br>
-            <input type="submit" name="search" value="Search"><br><br>
-            
-            <table>
-                <tr>
-                    <th>Rental Listing ID</th>
-                    <th>House Number</th>
-                    <th>Street Name</th>
-                    <th>City</th>
-                    <th>Rent</th>
-                    <th>Vacancies</th>
-                </tr>
-
-      <!-- populate table from mysql database -->
-                <?php while($row = mysqli_fetch_array($search_result)):?>
-                <tr>
-                    <td><?php echo $row['Rental_Listing_ID'];?></td>
-                    <td><?php echo $row['House_Number'];?></td>
-                    <td><?php echo $row['Street_Name'];?></td>
-                    <td><?php echo $row['City'];?></td>
-                    <td><?php echo $row['Rent_Per_Person'];?></td>
-                    <td><?php echo $row['Vacancies'];?></td>
-                </tr>
-                <?php endwhile;?>
-            </table>
-        </form>
-        
-    </body>
-</html>
+            echo '</table>';
+			/* close statement and connection*/ 
+			$stmt->close(); 
+			$mysqli->close();
+		?>
+		
+	<form action="searchListing.php" method="post"
+		<br>
+				<!-- The button for rate -->
+				<input type="submit" value="Rate"/>
+		</br>
+	</form>
+</body>
