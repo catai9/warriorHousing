255785b92050fd8c1e26fa77a34a5b56a1a3a2b1
sehersarin <sarin.seher@gmail.com>
2020-03-14 15:00:09 -0400

    change

--- my_connect.php
+++ my_connect.php
@@ -4,8 +4,8 @@ function get_mysqli_conn()
 {
 $dbhost = 'localhost';
 $dbuser = 'root';
-$dbpassword = '';
-$dbname = 'prdbondi';
+$dbpassword = 'root';
+$dbname = 'sksarin';
 #added comment tst
 
 $mysqli = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
