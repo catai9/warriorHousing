042ae2aa100e27d8b1210102f0e304a7517e7e5d
catai <catai@edu.uwaterloo.ca>
2020-03-15 12:14:49 -0400

    Added styling for buyer home functionality

--- buyerHome.php
+++ buyerHome.php
@@ -2,6 +2,7 @@
 
 <head>
   <title>Warrior Housing</title>
+  <link rel="stylesheet" href="styles/buyerHome.css">
 </head>
 <body>
 	<a href="login.php" class="button">Log Out</a>
@@ -10,15 +11,14 @@
 	<!-- Which page it will direct go upon submitting the form. -->
 	<!-- If the form submission is successful, it will redirect to its respective php file -->
 	<?php
+		
 		$user_id = $_POST["user_id"];
 		echo '<form action="searchFunctionality_input.php" method="post"> ';
 			echo '<input type="hidden" name="user_id" value="' . $user_id . '"/>'; 
-			echo '<br>
-					<!-- The button for search -->
-					<input type="submit" value="Search"/>
-				</br>
+			echo '
+				<input type="submit" class="orange" value="Search"/>
 		</form>';
-
+		echo '<div>';
 			// Enable error logging: 
 			error_reporting(E_ALL ^ E_NOTICE);
 			// mysqli connection via user-defined function
@@ -42,28 +42,31 @@
 			// Bind result variables 
 			$stmt->bind_result($rental_listing_ID, $city, $street_name, $house_number, $vacancies, $rent_per_person, $availability_length); 
 		
-			//printing output in html table
-			while ($stmt->fetch()) {
 			echo '<table style="width:100%">';
             echo '<tr>';
-                echo '<th>Rental Listing ID</th>';
-                echo '<th>Address</th>';
+                echo '<th class="address">Address</th>';
                 echo '<th>Vacancies</th>';
                 echo '<th>Rent Per Person</th>';
-                echo '<th>Length of Availability</th>';
-            echo '</tr>';
-            echo '<tr><td>' . $rental_listing_ID . '</td><td>' . $city . ', ' . $street_name .', ' . $house_number . '</td><td>'. $vacancies . '</td><td>'. $rent_per_person . '</td><td>'. $availability_length. '</td><td>';
-			echo '</table>';
-			echo '<form action="listingDetails.php" method="post">';
-			echo '<input type="hidden" name="user_id" value="' . $user_id . '"/>'; 
-			echo '<input type="hidden" name="rental_listing_ID" value="' . $rental_listing_ID . '"/>'; 
-			echo '<br>';
+				echo '<th>Availability Length (months)</th>';
+				echo '<th>More Details</th>';
+			echo '</tr>';
+
+			//printing output in html table
+			while ($stmt->fetch()) {
+				echo '<tr><td class="address">' . $house_number . ' ' . $street_name .', ' . $city . '</td><td>'. $vacancies . '</td><td>'. $rent_per_person . '</td><td>'. $availability_length. '</td>';
+				
+				echo '<td>';
+				echo '<form action="listingDetails.php" method="post">';
+				echo '<input type="hidden" name="user_id" value="' . $user_id . '"/>'; 
+				echo '<input type="hidden" name="rental_listing_ID" value="' . $rental_listing_ID . '"/>'; 
+				echo '<br>';
 				echo '<!-- The button for rate -->';
-				echo '<input type="submit" value="Further Details"/>';
-			echo '</br>';
-			echo '</form>';
+				echo '<input type="submit" class="green" value="See More"/>';
+				echo '</br>';
+				echo '</form></td>';
 			}
-			echo '<br><a href="login.php" class="button">Log Out</a></br>';
+			echo '</table>';
+			echo '</div>';
 			/* close statement and connection*/ 
 			$stmt->close(); 
 			$mysqli->close();
