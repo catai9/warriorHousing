ec581b041b22d0c3c8b8cfa06d22debc0d031c12
catai <catai@edu.uwaterloo.ca>
2020-03-14 22:00:07 -0400

    Styling completed for login and registration page

--- login.php
+++ login.php
@@ -3,27 +3,27 @@
 
 <head>
   <title>Warrior Housing</title>
+  <link rel="stylesheet" href="styles/formFormat.css">
 </head>
 
 <body>
     
     <h1>Warrior Housing</h1>
-    <h2>Login Page</h2>
     
     <div>
+        <h2>Login</h2>
         <!-- Directs to the loginCheck.php to check if the user successfully log ins.
          Uses post as it is more secure than get -->
         <form action="loginCheck.php" method="post">
-
-            <div>Enter email: </div>
+            <p>Enter email: </p>
             <!-- Stores the user input of email into a variable named email. -->
-            <input type="text" name="email" required/><br>
-            <div>Enter password: </div>
-            <input type="password" name="password" required/><br>
-            <input type="submit" name="submit" class="teal" value="Continue"/>
+            <input type="text" name="email" required/>
+            <p>Enter password: </p>
+            <input type="password" name="password" required/> <br><br>
+            <input type="submit" name="submit" class="teal" value="Log in"/> <br>
         </form>
-        <div>Need an account?</div>
-        <br><a href="index.php" class="button">Register</a></br>
+        Need an Account? <br>
+        <a href="index.php" class="button">Register</a>
     </div>
 
 </body>
