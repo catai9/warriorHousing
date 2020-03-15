77c4f1236dea31b38ae49853a8b4c9a8ff14996b
catai <catai@edu.uwaterloo.ca>
2020-03-15 17:24:16 -0400

    Updated styling for search functionality

--- searchFunctionality_input.php
+++ searchFunctionality_input.php
@@ -2,36 +2,34 @@
 
 <head>
   <title>Warrior Housing</title>
-  <link rel="stylesheet" href="styles/formFormat.css">
+  <link rel="stylesheet" href="styles/listing.css">
 </head>
 
 <body>
-	<h1>Warrior Housing</h1>
 	<?php
 		$user_id = $_POST["user_id"];
+		echo '<form action="buyerHome.php" method="post">
+        <input type="hidden" name="user_id" value="' . $user_id . '"/>
+        <input type="submit" class="link" value="Return to Home"/>
+		</form>';
+
+		echo '<h1>Warrior Housing</h1>';
 		echo '
-			
-
+			<div>
 				<form id="form11" action="searchFunctionality_search.php" method="post">		
-				<input type="hidden" name="user_id" value="' . $user_id . '"/>
-				<br>
-						Search by Street Name<br>
-						<input type="text" name="search"/>	<br>
-						<!-- The button for this form. -->
-						<input type="submit" value="Submit Search"/>
-					</br>
-					<br>
-						Search by City<br>
-						<input type="text" name="city"/>	<br>
-						<!-- The button for this form. -->
-						<input type="submit" value="Submit Search"/>
-					</br>
-				</form>
-
-				<form id="form3" action="buyerHome.php" method="post">
 					<input type="hidden" name="user_id" value="' . $user_id . '"/>
-					<input type="submit" value="Back To Home"/>
+					<br>
+						<p>Keywords:</p>
+						<input type="text" name="search" required/>
+						<p>Search By:</p>
+						<select id="category" name="category" required>
+							<option value="street">Street Name</option>
+							<option value="city">City</option>
+						</select> <br><br>
+						<!-- The button for this form. -->
+						<input type="submit" class="orange" value="Submit Search"/>
 				</form>
+			</div>
 		'
 	?>
 </body>
